9cb24f8fd95b8a5d868d6d5802c32f5968d0e380
Stanisław Jarocki <s.jarocki354@gmail.com>
2023-09-05 07:19:39 +0200

    add footer

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,19 +1,13 @@
 import { component$ } from "@builder.io/qwik";
-import { useServerTimeLoader } from "~/routes/layout";
-import styles from "./footer.module.css";
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
-
-  return (
-    <footer>
-      <div class="container">
-        <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Made with ♡ by Builder.io</span>
-          <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
-        </a>
-      </div>
-    </footer>
-  );
+    return (
+        <div class="flex justify-center gap-2 bg-emerald-500">
+            <p class="text-black" title="1. Memorization of selected developers even after app exit. 2.Auto-generated message with 'Hi...', screenshot as a proof & PR link ready to be copied and send on e.g. MS Teams. 3. Opportunity to bet on the outcome of the draw for as much as $100. 4. Better roll animation. 5. With point 3, I was just kidding.">
+                Incoming soon (hover me!)
+            </p>
+            <p class="text-black">|</p>
+            <p class="text-black">Created by @Stanlee77</p>
+        </div>
+    )
 });

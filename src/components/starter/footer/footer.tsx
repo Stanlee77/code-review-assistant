64e7b2c324131a3181194095f6a02ee23765ef5b
Stanisław Jarocki <s.jarocki354@gmail.com>
2023-09-05 07:25:54 +0200

    add tailwind

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -4,7 +4,7 @@ export default component$(() => {
     return (
         <div class="flex justify-center gap-2 bg-emerald-500">
             <p class="text-black" title="1. Memorization of selected developers even after app exit. 2.Auto-generated message with 'Hi...', screenshot as a proof & PR link ready to be copied and send on e.g. MS Teams. 3. Opportunity to bet on the outcome of the draw for as much as $100. 4. Better roll animation. 5. With point 3, I was just kidding.">
-                Incoming soon (hover me!)
+                Coming soon (hover me!)
             </p>
             <p class="text-black">|</p>
             <p class="text-black">Created by @Stanlee77</p>
